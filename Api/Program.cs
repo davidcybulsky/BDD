00d9652a2cfb5127937217a8a6186448dbc398de
sekulas <sekula.sebastian22@gmail.com>
2023-11-20 22:20:46 +0100

    Added sqlite connection

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,7 +1,11 @@
+using Api.Entities;
 using Api.Interfaces;
 using Api.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -9,16 +13,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddCors(options => {
     options.AddDefaultPolicy(
-        policy => {
-            policy.WithOrigins("http://localhost:3000")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-        });
+    policy => {
+        policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITicketService,  TicketService>();
+builder.Services.AddDbContext<SafariContext>(options =>
+{
+    options.UseSqlite(configuration.GetConnectionString("Default"));
 });
 
 var app = builder.Build();
