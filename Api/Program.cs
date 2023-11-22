365cba510e9f9e2d0460b43d0c18291464d77e0b
Dawid Cybulski <dawid.cybulsky@interia.pl>
2023-11-22 20:00:28 +0100

    Controllers for animals and caretakers were added

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,7 +2,6 @@ using Api.Entities;
 using Api.Interfaces;
 using Api.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -14,9 +13,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options => {
+builder.Services.AddCors(options =>
+{
     options.AddDefaultPolicy(
-    policy => {
+    policy =>
+    {
         policy.WithOrigins("http://localhost:3000")
         .AllowAnyHeader()
         .AllowAnyMethod()
@@ -25,7 +26,10 @@ builder.Services.AddCors(options => {
 });
 
 builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<ITicketService,  TicketService>();
+builder.Services.AddScoped<ITicketService, TicketService>();
+builder.Services.AddScoped<IAnimalService, AnimalService>();
+builder.Services.AddScoped<IExtendedAnimalService, AnimalService>();
+builder.Services.AddScoped<ICaretakerService, CaretakerService>();
 builder.Services.AddDbContext<SafariContext>(options =>
 {
     options.UseSqlite(configuration.GetConnectionString("Default"));
@@ -34,7 +38,7 @@ builder.Services.AddDbContext<SafariContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
